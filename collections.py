db622efcec4101eecce206bbaab989437cf37744
Joe Rezendes <rezenbiz@gmail.com>
2017-12-09 15:20:54 -0800

    Added collections

---
+++ collections.py
@@ -0,0 +1,10 @@
+arr = ['yellow', 'blue', 'green'];
+
+list = ['item0', 'item1', 'item2'];
+
+edible = {'Concrete': 'no', 'Hotdog': 'yes'};
+
+h = 'hash this'
+hash(h)
+
+tup = ('Make', 'School', 2017, 'Janurary');
