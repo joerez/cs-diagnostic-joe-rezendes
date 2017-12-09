355dbb646a6b7106f8c173d44657169baa55ff07
Joe Rezendes <rezenbiz@gmail.com>
2017-12-09 15:11:41 -0800

    added python functions

---
+++ functions.py
@@ -0,0 +1,25 @@
+#problem 6
+
+def fibonacci_iterative(num):
+    num = 10
+
+
+#problem 7
+
+import math
+def factorial_recursive(num):
+    math.factorial(num)
+    return num
+
+#problem 8
+
+An array stores values such as numbers, strings or booleans, that can be accessed by their position.
+Compared to an array lists, have some more things that can be done with them because of their built in properties.
+
+An array can only hold the same type of data, while a list can have different types.
+
+A hashtable is a way to implement a dictionary. A dictionary maps keys to values.
+
+A tuple is a sequence of unchangable objects
+
+#problem 9
